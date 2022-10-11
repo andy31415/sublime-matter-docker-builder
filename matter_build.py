328957dea585b6837aea8fa0968c9ec7ae9720c3
Andrei Litvin <andy314@gmail.com>
2022-10-11 19:22:23 -0400

    Update script to the latest build logic

--- matter_build.py
+++ matter_build.py
@@ -4,6 +4,7 @@ import sublime_plugin
 import subprocess
 import threading
 import os
+import os.path
 import re
 
 FILE_REGEX = r"^INFO *(.*[^:]):(\d+):(\d+): (?:fatal )?((?:error|warning): .+)$"
@@ -14,9 +15,9 @@ DOCKER_MAPPED_PATH="/workspace/"
 # DOCKER_EXEC_BASH_STR= "docker exec -w /workspace bld_vscode /bin/bash -c"
 DOCKER_EXEC_BASH_STR= "podman exec -w /workspace bld_vscode /bin/bash -c"
 
-
-GLOB_TARGET = "CUSTOM GLOB"
 RE_COMPUTE_TARGET = "RE-COMPUTE"
+
+TARGETS_PATH = os.path.join(sublime.cache_path(), 'matter_build_targets.list')
 
 class MatterDockerBuild(sublime_plugin.WindowCommand):
 
@@ -27,32 +28,33 @@ class MatterDockerBuild(sublime_plugin.WindowCommand):
     panel_lock = threading.Lock()
     build_targets = None
     last_selected_index = -1
-    last_glob_expression = ""
 
     def targets(self):
         if self.build_targets is None:
-            self.build_targets = []
-            for t in self.compute_build_targets():
-                if ' (NOGLOB' in t:
-                    idx = t.find(' (NOGLOB')
-                    trigger, annotation = t[:idx], t[idx + 2:]
-                    self.build_targets.append([trigger, annotation])
-                else:
-                    self.build_targets.append([t,])
+            self.compute_build_targets()
 
-            # Also allow custom globbing
-            self.build_targets.append([GLOB_TARGET, "Enter a custom glob using * and {} (NOT a regex!)"])
+            # Allow re-computation of all targets
             self.build_targets.append([RE_COMPUTE_TARGET, "Re-compute build targets"])
 
         return self.build_targets
 
     def compute_build_targets(self):
+        self.build_targets = []
+        if not os.path.exists(TARGETS_PATH):
+            self.recompute_build_targets()
+
+        with open(TARGETS_PATH, 'rt') as f:
+            for item in f.readlines():
+                self.build_targets.append([item.strip(), ])
+
+    def recompute_build_targets(self):
         items = subprocess.check_output(
                 DOCKER_EXEC_BASH_STR.split() +
-                ['source ./scripts/activate.sh >/dev/null 2>&1 && ./scripts/build/build_examples.py --log-level fatal targets']).split(b'\n')
+                ['source ./scripts/activate.sh >/dev/null 2>&1 && ./scripts/build/build_examples.py --log-level fatal targets --expand']).split(b'\n')
 
-        for item in items:
-            yield item.decode('utf8')
+        with open(TARGETS_PATH, 'wt') as f:
+            for item in items:
+                f.write('%s\n' % item.decode('utf8'))
 
 
     def is_enabled(self, kill=False):
@@ -69,20 +71,12 @@ class MatterDockerBuild(sublime_plugin.WindowCommand):
         self.last_selected_index = target_index
         target = self.targets()[target_index][0]
 
-        if target == GLOB_TARGET:
-            self.window.show_input_panel("Target GLOB expression", self.last_glob_expression,
-                                         on_done = self.target_glob_done,
-                                         on_change = None,
-                                         on_cancel = None)
-        elif target == RE_COMPUTE_TARGET:
+        if target == RE_COMPUTE_TARGET:
+            os.remove(TARGETS_PATH)
             self.build_targets = None
-            self.targets()
+            self.targets() # forces a recompute
         else:
             self.run_build(target)
-
-    def target_glob_done(self, target_glob):
-        self.last_glob_expression = target_glob
-        self.run_build(target_glob)
 
 
     def run(self, kill=False):
@@ -143,12 +137,7 @@ class MatterDockerBuild(sublime_plugin.WindowCommand):
 
         args = DOCKER_EXEC_BASH_STR.split()
 
-        # TODO: this would work if we would not be using the quickselect panel.
-        #       We should have a 'GLOB' in  quickselect which provides a glob prompt
-        if '*' in target or '{' in target or '?' in target:
-            target_str = '--target-glob "%s"' % target
-        else:
-            target_str = '--target "%s"' % target
+        target_str = '--target "%s"' % target
 
         args.append('source ./scripts/activate.sh && ./scripts/build/build_examples.py --enable-flashbundle --no-log-timestamps %s build' % target_str)
 
